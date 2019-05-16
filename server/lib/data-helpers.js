99990a6903e85b092db8f67136c68b502473021f
kcchaha <kcchaha@gmail.com>
2019-05-16 18:47:14 +0000

    mongodb activated, minor stylying changes, fix functional errors

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,19 +9,22 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
+      db.collection("tweets").insertOne(newTweet, (err, result) => {
+        if (err) {
+          console.log("Something exploded on POST /tweets!");
+        }
+        callback(err);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
   };
 }
