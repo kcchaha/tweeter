0822d585525565600cf9b69ab3e6d847bb2c1909
kcchaha <kcchaha@gmail.com>
2019-05-16 19:58:37 +0000

    scss enabled, sass middleware installed

--- server/index.js
+++ server/index.js
@@ -3,9 +3,20 @@
 // Basic express setup:
 
 const PORT          = 3000;
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'styles'),
+  dest: path.join('public', 'styles'),
+  debug: true,
+  // force: true,
+  outputStyle: 'compressed',
+  prefix:  '/styles', error: console.log// Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -26,19 +37,6 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     app.use("/tweets", tweetsRoutes);
   }
 });
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-
-// Mount the tweets routes at the "/tweets" path prefix:
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
